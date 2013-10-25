11854eee57a5cc434948f17d600cc71ed30247e6
Hovhannes Sargsyan <sarhov@sarhov.com>
2013-10-25 23:55:30 +0400

    update

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,45 @@
-jQuery(document).ready(function($) {
-	
+jQuery(document).ready(function ($) {
+
+    // prodcut other images
+
+    $(".item-other-images-car").carouFredSel({
+        auto: false,
+        prev: "#other-prev",
+        next: "#other-next"
+    });
+
+    // need products
+
+    $("#b-blocks-car").carouFredSel({
+        auto: false,
+        prev: "#b-toprev",
+        next: "#b-tonext"
+    });
+
+    // see also
+
+    $(".see-also-car").carouFredSel({
+        auto: false,
+        prev: "#also-prev",
+        next: "#also-next"
+    });
+
+    // product page fancybox
+
+	$(".fancybox").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+
+
+
 });
