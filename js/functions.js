39afd3ab16f1936ef293983611e8b88e726db8ec
Hovhannes Sargsyan <sarhov@sarhov.com>
2013-10-26 02:08:10 +0400

    update fancybox and new zoom

--- js/functions.js
+++ js/functions.js
@@ -40,6 +40,38 @@ jQuery(document).ready(function ($) {
 		}
 	});
 
+      //вешаем плагин на контейнер-картинку
+        $(".my-foto-container").imagezoomsl({
+	  
+     innerzoommagnifier: true,
+      classmagnifier: window.external ? window.navigator.vendor === "Yandex" ? "" : "round-loupe" : "",
+      magnifierborder: "5px solid #F0F0F0",                               // fix для Opera, Safary, Yandex		  
+      zoomrange: [2, 8],
+      zoomstart: 2,
+      magnifiersize: [150, 150]			
+  });
 
+	  
+      //клик по превью-картинке
+        $(".my-foto").click(function(){
+
+           var that = this;
+ $('.mine-image').attr("href",              $(that).attr("data-large"))  
+         //копируем атрибуты из превью-картинки в контейнер-картинку
+           $(".my-foto-container").fadeOut(600, function(){
+
+             $(this).attr("src",              $(that).attr("data-middle"))              // путь до small картинки
+                    .attr("data-large",       $(that).attr("data-large"))       // путь до big картинки
+					
+                    //дополнительные атрибуты, если есть
+                    //.attr("data-title",       $(that).attr("data-title"))       // заголовок подсказки
+                    //.attr("data-help",        $(that).attr("data-help"))        // текст подсказки    
+                    //.attr("data-text-bottom", $(that).attr("data-text-bottom")) // текст снизу картинки
+                    
+                    .fadeIn(1000);				
+           });
+         
+           return false;
+       });	 
 
 });
